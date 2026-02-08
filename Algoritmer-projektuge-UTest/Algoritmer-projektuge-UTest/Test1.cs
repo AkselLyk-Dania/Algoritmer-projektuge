e8a6dfd0d779784e3982ff9db10448e7c35c6b5c
AkselLyk-Dania <akly5@student.eadania.dk>
2026-02-08 17:44:46 +0100

    Test
    Testing

---
+++ Algoritmer-projektuge-UTest/Algoritmer-projektuge-UTest/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Algoritmer_projektuge_UTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
